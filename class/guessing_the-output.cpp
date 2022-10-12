fbcf420d8bdc97630034f4828406489a641f8e1c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-12 11:00:09 +0200

    *[new] : guessing the output exercise

---
+++ class/guessing_the-output.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	
+	int n1 = 5, n2 = 10, max;
+	max = (n1 > n2) ? n1 : n2;
+	
+	cout << "largest number between n1 and n1 is " << max <<endl;
+	
+	return 0;
+}
