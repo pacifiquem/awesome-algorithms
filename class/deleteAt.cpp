d32e7733b6df1664161da6f82c88ccf6a86ed32c
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-10-20 11:02:09 +0200

    [feat] deleteAt added

---
+++ class/deleteAt.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void deleteAt(int numberArray[], int index) {
+	
+	for(int i=0; i<sizeof(numberArray); i++) {
+		if( i >= index) {
+			numberArray[i] = numberArray[i+1];
+		}
+	}
+	
+	for(int i=0; i<sizeof(numberArray); i++) {
+		cout << "Element " << i+1 << ": " << numberArray[i] <<endl;
+	}
+}
+int main() {
+	
+	int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	deleteAt(array, 7);
+	
+	return 0;
+}
