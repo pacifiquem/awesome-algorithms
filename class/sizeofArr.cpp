75e9b4f7bfd2419359648c3476d1057c565919d4
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-04 21:44:20 +0200

    [refractor] sizeofArr.cpp modified

--- class/sizeofArr.cpp
+++ class/sizeofArr.cpp
@@ -7,7 +7,6 @@ int main() {
 	int a[40] = {1, 2, 3, 4, 5, 6};
 	int size = sizeof(a);
 	int elements = sizeof(a)/ sizeof(int);
-	int sizeBySize = a.size();
 	cout << "The size is :  " << size << " " << "Elements are : " << elements <<endl;
 	
 	cout << "Elements : ";
