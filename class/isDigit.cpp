95aea3d99c59124c37c049fe9a2c01a01704484a
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-11-04 21:43:51 +0200

    [feat] : isDigit.cpp added

---
+++ class/isDigit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int isDigit(int integer) {
+	
+	string integerString = to_string(integer);
+	char integerChars[5];
+	strcpy(integerChars, integerString.c_str());
+	char * integerArr = strtok(integerChars, "");
+
+}
+
+int main() {
+	
+	
+	
+	return 0;
+}
